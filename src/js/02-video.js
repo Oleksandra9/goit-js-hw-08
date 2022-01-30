4df89dafe83e663b8eab06d97e8d647717f78e78
Oleksandra9 <sashakrytska@gmail.com>
2022-01-30 19:42:45 +0000

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+function getTime(event) {
+  localStorage.videoplayerCurrentTime = event.seconds;
+  console.log(event);
+}
+
+player.on('timeupdate', throttle(getTime, 1000));
+
+player.setCurrentTime(localStorage.videoplayerCurrentTime);
+
+
+
 
