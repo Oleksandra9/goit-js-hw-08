c7742aca3a91ded7d88ee0c9e56180b195eb1c89
Oleksandra9 <sashakrytska@gmail.com>
2022-02-06 01:43:08 +0000

    task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector("form"),
+    inputEl: document.querySelector("input"),
+    messageEl: document.querySelector("textarea"),
+    
+}
+
+// console.log(refs.form);
+// console.log(refs.inputEl);
+// console.log(refs.messageEl);
+
+
+let formData = {};
+const STORAGE_KEY = "feedback-form-state"
+
+refs.form.addEventListener("input", throttle(onFormInput,500));
+refs.form.addEventListener("submit", onFormSubmit);
+checkLocalStorage();
+
+function onFormInput () {
+    formData = {
+        email: refs.inputEl.value,
+        message: refs.messageEl.value,
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    return formData;
+}
+
+function onFormSubmit (event) {
+    event.preventDefault();
+    console.log( JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    event.currentTarget.reset();
+    
+    localStorage.removeItem(STORAGE_KEY);
+    
+}
+
+function checkLocalStorage() {
+    const recordLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(recordLocalStorage);
+    if (recordLocalStorage) {        
+        refs.inputEl.value = recordLocalStorage.email;
+        refs.messageEl.value = recordLocalStorage.message;
+    }
+}
